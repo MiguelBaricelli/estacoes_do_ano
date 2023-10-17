3d4083bb58c75f361a8902021803b94468df6cda
HenriqueMiaciro <130669699+HenriqueMiaciro@users.noreply.github.com>
2023-10-17 14:06:08 -0300

    estações ano miguel

--- teste.py
+++ teste.py
@@ -1,7 +1,22 @@
-#comentario miguel
+#Entrada
 
-print('Boa tarde a todos')
+print('    Estações do ano    ')
 
-a = input('Qual o seu nome?: ')
+a = input('Escreva a estação do dia que deseja: ')
+aa = input('Escreva o mês que deseja: ')
+a.title()
 
-print('Bem vindo(a)', a)
+b = '20''março'
+c = '21''junho'
+d = '23''setembro'
+e = '22''dezembro'
+
+if a and aa in b:
+    print('outono - 18:25h')
+if a in c:
+    print('inverno- 03:50h')
+if a in d:
+    print('primavera - 11:58h')
+if a in e:
+    print('verão - 00:27h')
+
