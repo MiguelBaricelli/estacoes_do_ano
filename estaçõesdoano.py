97e5131f07ca8c704d4b4687949d7a41f7cfc84c
HenriqueMiaciro <130669699+HenriqueMiaciro@users.noreply.github.com>
2023-10-18 13:23:14 -0300

    estações do ano miguel novo 2!

--- estaçõesdoano.py
+++ estaçõesdoano.py
@@ -6,13 +6,30 @@ outono = [4, 5, 6]
 inverno = [7, 8, 9]
 primavera = [10, 11, 12]
 
-if mes in verao and dia <= 21:
+if mes in verao and dia <= 19:
     print('Verão')
-if mes in outono and dia <= 21:
+elif mes == 3 and dia >= 20:
     print('Outono')
-if mes in inverno and dia <= 21:
+else:
+    print('erro')
+
+if mes in outono:
+    print('Outono')
+elif mes == 6 and dia <= 21:
     print('Inverno')
-if mes in primavera and dia <= 21:
+else:
+    print('Erro')
+
+if mes in inverno and dia <= 22:
+    print('Inverno')
+elif mes == 9 and dia >= 23:
     print('Primavera')
 else:
-    print()
+    print('Erro')
+
+if mes in primavera and dia <= 21:
+    print('Primavera')
+elif mes == 12 and dia >= 22:
+    print('Verão')
+else:
+    print('Erro')
